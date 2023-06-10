677e1f1bf960316316e2215d4175e019414874bb
“dakinnir” <dakinnir@gmail.com>
2023-06-10 13:39:42 -0400

    include fontsource styles

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import '@fontsource/montserrat';
+
+const Home = () => {
+  return (
+    <div style={{ fontFamily: "Montserrat, sans-serif"}}>
+      <h1 className='font-bold'>Welcome to the homepage!</h1>
+    </div>
+  );
+};
+
+export default Home;
